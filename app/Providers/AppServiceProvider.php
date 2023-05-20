57317706820fd26b828062fe504a2cb7a5c265f5
perdomo <juanj.perdomov@uqvirtual.edu.co>
2023-05-19 22:43:36 -0500

    Se activa protocolo https para protocolo de produccion

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,7 +20,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //URL::forceScheme('https');
+        URL::forceScheme('https');
         Schema::defaultStringLength(191);
 
         User::created(function($user) {
