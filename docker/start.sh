a8a3b7473958cd1124d8b72a4c3b244cdcfe7c08
perdomo <juanj.perdomov@uqvirtual.edu.co>
2023-05-29 22:18:23 -0500

    Se actualizaron las variables de la base de datos

--- docker/start.sh
+++ docker/start.sh
@@ -3,6 +3,6 @@ composer install
 # Inicia PHP-FPM en segundo plano
 php-fpm &
 php artisan migrate
-php artisan passport:key   
+php artisan passport:keys
 # Inicia Nginx en primer plano
 nginx -g "daemon off;"
